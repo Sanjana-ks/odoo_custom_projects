12f489c9c56aa77f928b86dd96d96cb3b42c634a
Sanjana-ks <sanjana.gharewal@ksolves.com>
2023-10-05 19:40:25 +0530

    manifest updating

--- practice_module/__manifest__.py
+++ practice_module/__manifest__.py
@@ -5,7 +5,7 @@
     'version': '16.1.1',
     'sequence': '1',
     'category': 'Git Submodule Check ',
-    'summary': 'Learning Odoo',
+    'summary': 'Learning Odoo and Github Submodulessssssssssssssssssssss',
     'description': 'This is the practice module on working with odoo. Testing this module.',
     'depends': ["base", 'report_xlsx'],
     'data': [
