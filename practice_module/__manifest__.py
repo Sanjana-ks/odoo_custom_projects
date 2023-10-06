93eecbe9d34bbacf79900b546ae728d3465780bd
Sanjana-ks <sanjana.gharewal@ksolves.com>
2023-10-06 18:09:14 +0530

    success for multiple repo

--- practice_module/__manifest__.py
+++ practice_module/__manifest__.py
@@ -6,7 +6,7 @@
     'sequence': '1',
     'category': 'Git Submodule Check ',
     'summary': 'Learning Odoo and Github Submodules & Actions.',
-    'description': 'Github Action created for multi-repo!!',
+    'description': 'Github Action created for multiple child repo successfully!!',
     'depends': ["base", 'report_xlsx'],
     'data': [
         "security/ir.model.access.csv",
